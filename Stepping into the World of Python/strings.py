eff0fa5aa236f7e867a6f9f0000cc4317cc1e4eb
a9na <anaiscoding@gmail.com>
2024-04-26 00:26:49 +0200

    commit

--- Stepping into the World of Python/strings.py
+++ Stepping into the World of Python/strings.py
@@ -19,3 +19,8 @@ print(type(age))
 age_in_words = "2 years"
 print(name + " is " + age + " years old")
 print(type(age))
+
+
+print(f"Pi is approximately {22 / 7:12.50f}")
+pi = 22 / 7
+print(f"Pi is approximately {pi:12.50f}")
--- Stepping into the World of Python/strings2.py
+++ Stepping into the World of Python/strings2.py
@@ -45,3 +45,10 @@ print(parrot[:6] + parrot[6:])
 print(parrot[:])
 
 letters = "abcdefghijklmnopqrstuvwxyz"
+
+number = "9,223;372:036 854,775;807"
+seperators = number[1::4]
+print(seperators)
+
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val) for val in values])
